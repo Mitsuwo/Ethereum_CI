f04102a47b8397205997b12c190b7e6229849342
Your Name <hapowalp@gmail.com>
2018-11-15 19:15:52 +0900

    done

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,11 +1,24 @@
 pragma solidity ^0.4.24;
 
 contract Election {
-    // Store candidate
-    // Read candidate
-    string public candidate;
-    // Constructor
-    function Election () public {
-        candidate = "Candidate 1";
+
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    mapping(uint => Candidate) public candidates;
+
+    uint public candidatesCount;
+
+    constructor() public {
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate (string _name) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 }
