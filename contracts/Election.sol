9bc11a0dd26ad86165a0d5669ff71f74864f03e7
Your Name <hapowalp@gmail.com>
2018-11-15 19:50:29 +0900

    done

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -7,6 +7,8 @@ contract Election {
         string name;
         uint voteCount;
     }
+    
+    mapping(address => bool) public voters;
 
     mapping(uint => Candidate) public candidates;
 
@@ -20,5 +22,16 @@ contract Election {
     function addCandidate (string _name) private {
         candidatesCount ++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote (uint _candidateId) public {
+        //require
+        require(!voters[msg.sender]);
+        //require
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+        //record that voter has voted
+        voters[msg.sender] = true;
+        //update candidate vote Count
+        candidates[_candidateId].voteCount ++;
     }
 }
